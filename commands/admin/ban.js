412571caaa1b9b6931fc8c2c2e64ea48fc1d2a91
Lazysensy <mackenzie.irelandmain@gmail.com>
2021-03-14 18:28:21 +1100

    Change the ban command

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -11,11 +11,13 @@ module.exports =
     {
         const toBan = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args[0]);
         
-        const reason = args.slice(1).join(" ") || "There was no reason!";
+        const reason = args.slice(1).join(" ")
+
+        if(!reason) return message.reply(`Please use a reason`)
 
         const embed = new Discord.MessageEmbed()
         .setTitle(`**__You Have Banned__**`)
-        .setDescription(`You have been banned from the LazySensy Community \n\n **Reason**: ${reason} \n **Moderator**: ${message.author.username} \n **Date**: ${new Date().toLocaleDateString()} \n`)
+        .setDescription(`You have been banned from the LazySensy Community \n\n **Reason**: ${reason} \n **Moderator**: ${message.author.username} \n **Date**: ${new Date().toLocaleDateString()} \n\n If you feel like this is incorrect you can request an appeal through this link (https://forms.gle/tsiEJQdUSdwabjSc6)`)
         .setAuthor(`LazyBot`, 'https://i.imgur.com/lg74UJw.jpeg')
         .setTimestamp()
 
